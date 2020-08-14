2e98d23bcf5a4893a178c10189fa6e4ad2bb6c9e
RennyFrancoRivero <69353080+RennyFrancoRivero@users.noreply.github.com>
2020-08-14 10:15:50 -0400

    Add files via upload

---
+++ operaciones con numeros.cpp
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c,d,e,f,g;
+    cout<<"Ingresar primer termino";
+    cint>>a;
+    cout<<"Ingresar segundo termino";
+    cout>>b;
+    c=a+b;
+    cout<<"La suma es: "<<c<<endl;
+    d=a-b;
+    cout<<"La resta es: "<<d<<endl;
+    e=a*b;
+    cout<<"La multiplicacion es: "<<e<<endl;
+    f=a%b;
+    cout<<"El porcentaje es: "<<f<<endl;
+    g=a/b;
+    cout<<"La division es: "<<g<<endl;
+    
+}
