a88f37aafcecb06001ca31b29e0342a863f433bd
RennyFrancoRivero <69353080+RennyFrancoRivero@users.noreply.github.com>
2020-08-18 10:08:11 -0400

    Add files via upload

---
+++ Calculo de salario.cpp
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    float j,d,s;
+    cout<<"Ingrese la cantidad de dias: "<<endl;
+    cin>>d;
+    cout<<"Ingrese el monto de la jornada: "<<endl;
+    cin>>j;
+    s<-j*d;
+    cout<<"El salario es: "<< s << endl;
+    return 0;
+}
